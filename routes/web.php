344ce4bc002e5b5c96d4320b64b84267e0ca1940
Abernabern1 <Abernabern1@gmail.com>
2020-09-10 17:04:00 +0300

    Created IndexController and added index route path

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\IndexController;
+
+
+Route::get('/', [IndexController::class, 'index'])->name('index');
+
+
