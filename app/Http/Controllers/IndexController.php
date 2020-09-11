9d519f77e0384c3b405644b991c989e94f1ffef0
Abernabern1 <Abernabern1@gmail.com>
2020-09-11 18:52:24 +0300

    Added IndexController

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Advantage;
+use App\Models\Course;
+use App\Models\Message;
+use App\Models\Subscriber;
+use App\Models\Teacher;
+
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $courses = Course::all();
+        $teachers = Teacher::all();
+        $advantages = Advantage::all();
+        $mainAdvantages = Advantage::limit(2)->get();
+
+        return view('index', compact('advantages', 'courses', 'teachers', 'mainAdvantages'));
+    }
+
+    public function register(Request $request, User $user)
+    {
+        $user->fill($request->all())->save();
+
+        return redirect()->route('index');
+    }
+
+    public function message(Request $request, Message $message)
+    {
+        $message->fill($request->all())->save();
+
+        return redirect()->route('index');
+    }
+
+    public function subscribe(Request $request, Subscriber $subscriber)
+    {
+        $subscriber->fill($request->all())->save();
+
+        return redirect()->route('index');
     }
 }
