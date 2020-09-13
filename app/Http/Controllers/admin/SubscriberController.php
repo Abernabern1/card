5204f99e5c897fe1a82522dffebb4d489e5fb35b
Abernabern1 <Abernabern1@gmail.com>
2020-09-13 20:47:34 +0300

    Added Subscriber controller/view

--- app/Http/Controllers/admin/SubscriberController.php
+++ app/Http/Controllers/admin/SubscriberController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
 
 class SubscriberController extends Controller
 {
-    //
+    public function index()
+    {
+        $subscribers = Subscriber::all();
+
+        return view('admin.subscriber.index', compact('subscribers'));
+    }
+
+    public function delete($id)
+    {
+        Subscriber::destroy($id);
+
+        return redirect()->back()->with('status-success', 'Subscriber item is successfully deleted.');
+    }
 }
---
+++ resources/views/admin/subscriber/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+<div class="layout-content">
+  <div class="layout-content-body">
+    <div class="doc">
+      <!-- START OVERVIEW -->
+      <div class="doc-section">
+        <h1 id="overview" class="doc-heading">Subscribers</h1><br/>
+
+        @if(session('status-success'))
+          <div>
+              <span class="status">{{ session('status-success') }}</span>
+          </div><br>
+        @endif
+        <table class="table table-hover table-dark table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Эмейл</th>
+              <th scope="col">Действие</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($subscribers as $key=>$subscriber)
+            <tr>
+              <td>{{ $key+1 }}</td>
+              <td>{{ $subscriber->email }}</td>
+              <td class="sssss">
+                &nbsp&nbsp&nbsp
+                <form class="table-form" action="{{ route('admin.subscriber.delete', $subscriber->id) }}" method="post">
+                    <button class="table-button"><i class="fa fa-times-circle fa-lg red"></i></button>
+                    {{ method_field('DELETE') }}
+                    {{ csrf_field() }}
+                </form>
+            </tr>
+            @endforeach
+
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+@stop
