0d0c80441f423e3e26a7ed381722ffe75210963b
Abernabern1 <Abernabern1@gmail.com>
2020-09-13 20:46:14 +0300

    Added client controller/view

--- app/Http/Controllers/admin/ClientController.php
+++ app/Http/Controllers/admin/ClientController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    //
+    public function index()
+    {
+        $clients = Client::all();
+
+        return view('admin.client.index', compact('clients'));
+    }
+
+    public function delete($id)
+    {
+        Client::destroy($id);
+
+        return redirect()->back()->with('status-success', 'Client item is successfully deleted.');
+    }
 }
---
+++ resources/views/admin/client/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+<div class="layout-content">
+  <div class="layout-content-body">
+    <div class="doc">
+      <!-- START OVERVIEW -->
+      <div class="doc-section">
+        <h1 id="overview" class="doc-heading">Clients</h1><br/>
+
+        @if(session('status-success'))
+          <div>
+              <span class="status">{{ session('status-success') }}</span>
+          </div><br>
+        @endif
+        <table class="table table-hover table-dark table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Имя</th>
+              <th scope="col">Фамилия</th>
+              <th scope="col">Эмейл</th>
+              <th scope="col">Действие</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($clients as $key=>$client)
+            <tr>
+              <td>{{ $key+1 }}</td>
+              <td>{{ $client->first_name }}</td>
+              <td>{{ $client->last_name }}</td>
+              <td>{{ $client->email }}</td>
+              <td class="sssss">
+                &nbsp&nbsp&nbsp
+                <form class="table-form" action="{{ route('admin.client.delete', $client->id) }}" method="post">
+                    <button class="table-button"><i class="fa fa-times-circle fa-lg red"></i></button>
+                    {{ method_field('DELETE') }}
+                    {{ csrf_field() }}
+                </form>
+            </tr>
+            @endforeach
+
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+@stop
