af2c38883fab41694e41210c2cd0696ac793a3cf
Abernabern1 <Abernabern1@gmail.com>
2020-09-13 19:22:55 +0300

    Created Admin-Course view

---
+++ resources/views/admin/course/index.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+<div class="layout-content">
+  <div class="layout-content-body">
+    <div class="doc">
+      <!-- START OVERVIEW -->
+      <div class="doc-section">
+        <h1 id="overview" class="doc-heading">Overview</h1><br/>
+
+        @if(session('status-success'))
+          <div>
+              <span class="status">{{ session('status-success') }}</span>
+          </div><br>
+        @endif
+        <table class="table table-hover table-dark table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Название</th>
+                <th scope="col">Текст</th>
+                <th scope="col">Цена</th>
+                <th scope="col">Изображение</th>
+                <th scope="col">Студенты</th>
+                <th scope="col">Уроки</th>
+                <th scope="col">Недели</th>
+              <th scope="col">Действие</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($courses as $key=>$course)
+            <tr>
+              <td>{{ $key+1 }}</td>
+              <td>{{ $course->title }}</td>
+                <td>{{ $course->text }}</td>
+                <td>{{ $course->price }}</td>
+                <td>{{ $course->image }}</td>
+                <td>{{ $course->student_count }}</td>
+                <td>{{ $course->lesson_count }}</td>
+                <td>{{ $course->week_count }}</td>
+              <td class="sssss">
+                &nbsp&nbsp&nbsp
+                <a href="{{ route('admin.course.update', $course->id) }}">
+                  <i class="fa fa-cog fa-lg" style="color: black;"></i>
+                </a>
+                <form class="table-form" action="{{ route('admin.course.delete', $course->id) }}" method="post">
+                    <button class="table-button"><i class="fa fa-times-circle fa-lg red"></i></button>
+                    {{ method_field('DELETE') }}
+                    {{ csrf_field() }}
+                </form>
+            </tr>
+            @endforeach
+
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+@stop
---
+++ resources/views/admin/course/update.blade.php
@@ -0,0 +1,114 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="layout-content">
+    <div class="layout-content-body">
+        <div class="doc">
+            <!-- START OVERVIEW -->
+            <div class="doc-section">
+                <h1 id="overview" class="doc-heading">Menu</h1><br/>
+
+                @if($errors->all())
+                    <div class="errors">
+                        @foreach($errors->all() as $error)
+                            <p>{{ $error }}</p>
+                        @endforeach
+                    </div>
+                @endif
+                @if(session('status-success'))
+                    <div>
+                        <a class="status" href="{{ route('admin.course.index') }}">{{ session('status-success') }}</a>
+                    </div>
+                @endif
+
+                <form class="form-horizontal" method="POST" action="{{ route('admin.course.update', $course->id) }}"
+                      enctype="multipart/form-data">
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Title</label>
+                        <div class="col-sm-10">
+                            @if(old('title'))
+                                <input type="text" class="form-control" name="title" placeholder="Title" value="{{ old('title') }}">
+                            @else
+                                <input type="text" class="form-control" name="title" placeholder="Title" value="{{ $course->title }}">
+                            @endif
+                        </div>
+                    </div><br/>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Text</label>
+                        <div class="col-sm-10">
+                            @if(old('text'))
+                                <input type="text" class="form-control" name="text" placeholder="Text" value="{{ old('text') }}">
+                            @else
+                                <input type="text" class="form-control" name="text" placeholder="Text" value="{{ $course->text }}">
+                            @endif
+                        </div>
+                    </div><br/>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Price</label>
+                        <div class="col-sm-10">
+                            @if(old('price'))
+                                <input type="text" class="form-control" name="price" placeholder="Price" value="{{ old('price') }}">
+                            @else
+                                <input type="text" class="form-control" name="price" placeholder="Price" value="{{ $course->price }}">
+                            @endif
+                        </div>
+                    </div><br/>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Students</label>
+                        <div class="col-sm-10">
+                            @if(old('student_count'))
+                                <input type="text" class="form-control" name="student_count" placeholder="Students" value="{{ old('student_count') }}">
+                            @else
+                                <input type="text" class="form-control" name="student_count" placeholder="Students" value="{{ $course->student_count }}">
+                            @endif
+                        </div>
+                    </div><br/>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Lessons</label>
+                        <div class="col-sm-10">
+                            @if(old('lesson_count'))
+                                <input type="text" class="form-control" name="lesson_count" placeholder="Lessons" value="{{ old('lesson_count') }}">
+                            @else
+                                <input type="text" class="form-control" name="lesson_count" placeholder="Lessons" value="{{ $course->lesson_count }}">
+                            @endif
+                        </div>
+                    </div><br/>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Weeks</label>
+                        <div class="col-sm-10">
+                            @if(old('week_count'))
+                                <input type="text" class="form-control" name="week_count" placeholder="Weeks" value="{{ old('week_count') }}">
+                            @else
+                                <input type="text" class="form-control" name="week_count" placeholder="Weeks" value="{{ $course->week_count }}">
+                            @endif
+                        </div>
+                    </div><br/>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Image</label>
+                        <div class="col-sm-10">
+                            <input type="file" name="image">
+                        </div>
+                    </div><br/>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-default" style="margin-left: 75px;">Подтвердить</button>
+                    </div><br/>
+
+                    {{ method_field('PATCH') }}
+                    {{ csrf_field() }}
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+@stop
