ba148191266f93cf6ed5a11db7848d21cdcd8f34
Abernabern1 <Abernabern1@gmail.com>
2020-09-13 20:47:04 +0300

    Added subscriber controller/view

--- app/Http/Controllers/admin/MessageController.php
+++ app/Http/Controllers/admin/MessageController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    public function index()
+    {
+        $messages = Message::all();
+
+        return view('admin.message.index', compact('messages'));
+    }
+
+    public function delete($id)
+    {
+        Message::destroy($id);
+
+        return redirect()->back()->with('status-success', 'Message item is successfully deleted.');
+    }
 }
---
+++ resources/views/admin/message/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+<div class="layout-content">
+  <div class="layout-content-body">
+    <div class="doc">
+      <!-- START OVERVIEW -->
+      <div class="doc-section">
+        <h1 id="overview" class="doc-heading">Messages</h1><br/>
+
+        @if(session('status-success'))
+          <div>
+              <span class="status">{{ session('status-success') }}</span>
+          </div><br>
+        @endif
+        <table class="table table-hover table-dark table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Имя</th>
+              <th scope="col">Фамилия</th>
+              <th scope="col">Эмейл</th>
+              <th scope="col">Тема</th>
+              <th scope="col">Текст</th>
+              <th scope="col">Действие</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($messages as $key=>$message)
+            <tr>
+              <td>{{ $key+1 }}</td>
+              <td>{{ $message->first_name }}</td>
+              <td>{{ $message->last_name }}</td>
+              <td>{{ $message->email }}</td>
+              <td>{{ $message->subject }}</td>
+              <td>{{ $message->text }}</td>
+              <td class="sssss">
+                &nbsp&nbsp&nbsp
+                <form class="table-form" action="{{ route('admin.message.delete', $message->id) }}" method="post">
+                    <button class="table-button"><i class="fa fa-times-circle fa-lg red"></i></button>
+                    {{ method_field('DELETE') }}
+                    {{ csrf_field() }}
+                </form>
+            </tr>
+            @endforeach
+
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+@stop
